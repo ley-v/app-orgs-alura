4929b90a60656578082be58baac1e1eaf5d30cb7
ley-v <83968941+ley-v@users.noreply.github.com>
2021-07-01 16:45:12 +0100

    Implementando o layout do produto com ConstraintLayout

--- app/src/main/java/alura/orgs/MainActivity.kt
+++ app/src/main/java/alura/orgs/MainActivity.kt
@@ -12,12 +12,7 @@ class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        Toast.makeText(this, "testando o Toast", Toast.LENGTH_SHORT).show()
-
-        val view = TextView(this)
-        view.setText("criando o primeiro layout dentro da activity")
-
-        setContentView(view)
+        setContentView(R.layout.activity_main)
 
     }
 }
